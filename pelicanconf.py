bfdece58b1e6a54af03015bc50cee962dfbde329
Ludovic Coues <gkr@jellycopter.net>
2017-02-11 18:01:50 +0100

    Create a pelican site with an iridescent template

---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+from os import path
+
+AUTHOR = u'Hypatia'
+SITENAME = u'Transmisoginy'
+SITEURL = ''
+THEME = path.join(path.dirname(__file__), 'iridescent')
+print(THEME)
+
+PATH = 'content'
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+MENUITEMS = (("Testimony", "%s/blog.html" % SITEURL),)
+
+DEFAULT_PAGINATION = 10
+
+INDEX_SAVE_AS = "blog.html"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
